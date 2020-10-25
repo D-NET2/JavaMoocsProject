31303e31b287ffd8b557882851ba6614c70b3030
Dahir <61761709+D-NET2@users.noreply.github.com>
2020-10-25 10:47:37 +0000

    Delete FromHundredToOne.java

--- FromHundredToOne.java
+++
@@ -1,9 +0,0 @@
-public class FromHundredToOne{
-    public static void main(String args[]){
-        int num1 =100;
-        while((num1 < 102) && (num1 >= 1)){
-            System.out.println(num1);
-            num1--;
-        }
-    }
-}
